1f00a4959557f119645ad7c571de34b191280f63
cmd08010 <colleendunion@gmail.com>
2020-01-18 11:56:33 -0500

    finished the even number generator

--- src/Components/Numbers.js
+++ src/Components/Numbers.js
@@ -1,13 +1,60 @@
-import React from "react"
+import React, { useState } from "react"
+import faker from "faker"
 
-const Number = () => {
+const Numbers = () => {
+  let [numberArray, setNumberArray] = useState([])
+  let [checkbox, setCheckbox] = useState(true)
+
+  const handleCheck = e => {
+    checkbox = e.target.checked
+    setCheckbox(checkbox)
+  }
+
+  const addNumbers = () => {
+    let number
+    // console.log(Math.floor(Math.random() * 10) / 2)
+    if (checkbox === true) {
+      number = Math.floor(Math.random() * 10) * 2
+    } else {
+      number = Math.floor(Math.random() * 10)
+    }
+
+    // numberArray.push(number)
+    // numberArray.join(<br />)
+
+    setNumberArray([number, ...numberArray])
+  }
+
+  const getNumberLength = () => {
+    return numberArray.length
+  }
+
+  const onSubmit = e => {
+    e.preventDefault()
+  }
+
+  const numbersRendered = numberArray.map(num => (
+    <p className="card-text">{num}</p>
+  ))
+
   return (
-    <form>
-      <input name="test" type="text" />
-      <button type="submit">click me</button>
-      <p>"Start Typing!"</p>
-    </form>
+    <div>
+      <div className="card">
+        <p className="card-text">{getNumberLength()} unique numbers</p>
+      </div>
+
+      <form onSubmit={onSubmit}>
+        <input type="checkbox" checked={checkbox} onChange={handleCheck} /> Is
+        Even?
+        <div>
+          <button type="submit" onClick={addNumbers}>
+            Generate Number
+          </button>
+        </div>
+      </form>
+      <div className="card">{numbersRendered}</div>
+    </div>
   )
 }
 
-export default Number
+export default Numbers
