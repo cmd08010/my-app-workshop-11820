5690a3a80bf6c3de19b244ca4121e18bcac358cd
cmd08010 <colleendunion@gmail.com>
2020-01-18 10:19:15 -0500

    initial commit

---
+++ src/Components/Numbers.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+const Number = () => {
+  return (
+    <form>
+      <input name="test" type="text" />
+      <button type="submit">click me</button>
+      <p>"Start Typing!"</p>
+    </form>
+  )
+}
+
+export default Number
